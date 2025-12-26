8142349ccf5b0fa0f9e0f91747581d6945acef04
Mark Langen <stravant@gmail.com>
2025-12-25 23:24:48 -0800

    Export types from packages

--- Packages/Maid.lua
+++ Packages/Maid.lua
@@ -1,1 +1,4 @@
-return require(script.Parent._Index["nightcycle_maid@1.1.4"]["maid"])
+local REQUIRED_MODULE = require(script.Parent._Index["nightcycle_maid@1.1.4"]["maid"])
+export type Maid = REQUIRED_MODULE.Maid 
+export type MaidConstructor = REQUIRED_MODULE.MaidConstructor 
+return REQUIRED_MODULE
--- Packages/_Index/jsdotlua_roact-compat@17.2.1/react-roblox.lua
+++ Packages/_Index/jsdotlua_roact-compat@17.2.1/react-roblox.lua
@@ -1,1 +1,3 @@
-return require(script.Parent.Parent["jsdotlua_react-roblox@17.2.1"]["react-roblox"])
+local REQUIRED_MODULE = require(script.Parent.Parent["jsdotlua_react-roblox@17.2.1"]["react-roblox"])
+export type RootType = REQUIRED_MODULE.RootType 
+return REQUIRED_MODULE
